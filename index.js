2c1cb07167a9a227a754443f8dd4b91393ad2930
Nathan Berhanu <natib1928@gmail.com>
2025-04-09 16:20:54 +0300

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-let humanScore = 0;
-let compScore = 0;
+const score = {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+
+const buttons = document.querySelectorAll("button");
+const AIChoice = document.createElement("p");
+document.body.appendChild(AIChoice);
+
+const scoreDisplay = document.createElement("p");
+document.body.appendChild(scoreDisplay);
 
 function getComputerChoice() {
   let choiceNum = Math.random();
-  let compChoice = "Rock";
+  let compChoice = "";
 
   if (choiceNum < 0.33) {
     compChoice = "Rock";
@@ -16,68 +26,77 @@ function getComputerChoice() {
   return compChoice;
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Choose your hand?\n1. Rock\n2. Paper\n3. Scissors");
-    humanChoice = humanChoice.toLowerCase().trim();
-  if (humanChoice === "rock") {
-    return "Rock";
-  } else if (humanChoice === "paper") {
-    return "Paper";
-  } else {
-    return "Scissors";
+function updateScore() {
+  scoreDisplay.textContent = `Your Score ${score.wins} Computer Score ${score.losses} Ties ${score.ties}`;
+  if(score.wins == 5){
+    scoreDisplay.textContent = 'You win!';
+    clearGame()
+  } else if(score.losses == 5) {
+    scoreDisplay.textContent = 'You lose!';
+    clearGame()
   }
 }
+
+function clearGame(){
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  AIChoice.textContent = '';
+
+}
+
+buttons.forEach(button => {
+  button.addEventListener("click", (event) => {
+    const targetButton = event.currentTarget;
+
+    if (targetButton.classList.contains("restart")) {
+      clearGame();
+      scoreDisplay.textContent = `Your Score ${score.wins} Computer Score ${score.losses} Ties ${score.ties}`;
+      return;
+    }
+
+    const humanChoice = targetButton.classList.contains("rock")
+      ? "Rock"
+      : targetButton.classList.contains("paper")
+      ? "Paper"
+      : "Scissors";
+
+    playRound(humanChoice, getComputerChoice());
+  });
+});
 
 function playRound(humanChoice, compChoice) {
   if (humanChoice === "Rock" && compChoice === "Scissors") {
-    console.log("You choose Rock");
-    console.log("Computer chooses Scissors");
-    console.log("You win!");
-    humanScore++;
+    AIChoice.textContent = `Computer Chose: ${compChoice}`;
+    score.wins += 1;
+
   } else if (humanChoice === "Rock" && compChoice === "Paper") {
-    console.log("You choose Rock");
-    console.log("Computer chooses Paper");
-    console.log("You lose!");
-    compScore++;
+    AIChoice.textContent = `Computer Chose: ${compChoice}`;
+    score.losses += 1;
+
   } else if (humanChoice === "Paper" && compChoice === "Scissors") {
-    console.log("You choose Paper");
-    console.log("Computer chooses Scissors");
-    console.log("You lose!");
-    compScore++;
+    AIChoice.textContent = `Computer Chose: ${compChoice}`;
+    score.losses += 1;
+    
   } else if (humanChoice === "Paper" && compChoice === "Rock") {
-    console.log("You choose Paper");
-    console.log("Computer chooses Rock");
-    console.log("You win!");
-    humanScore++;
+    AIChoice.textContent = `Computer Chose: ${compChoice}`;
+    score.wins += 1;
+
   } else if (humanChoice === "Scissors" && compChoice === "Rock") {
-    console.log("You choose Scissors");
-    console.log("Computer chooses Rock");
-    console.log("You lose!");
-    compScore++;
+    AIChoice.textContent = `Computer Chose: ${compChoice}`;
+    score.losses += 1;
+    
   } else if (humanChoice === "Scissors" && compChoice === "Paper") {
-    console.log("You choose Scissors");
-    console.log("Computer chooses Paper");
-    console.log("You win!");
-    humanScore++;
+    AIChoice.textContent = `Computer Chose: ${compChoice}`;
+    score.wins += 1;
+    
   } else if (humanChoice === compChoice) {
-    console.log(`You choose ${humanChoice}`);
-    console.log(`Computer chooses ${compChoice}`);
-    console.log("Draw");
+    AIChoice.textContent = `Computer Chose: ${compChoice}`;
+    score.ties += 1;
   }
-  console.log(`Score You: ${humanScore} Computer: ${compScore}`);
+
+  
+  updateScore();
 }
 
-function playGame(){
-  for(i=0; i<=4; i++){
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-  if (humanScore > compScore) {
-    console.log(`You have won the game ${humanScore} - ${compScore}`);
-  } else if (humanScore < compScore) {
-    console.log(`You have lost the game ${humanScore} - ${compScore}`);
-  } else {
-    console.log(`The game is a draw ${humanScore} - ${compScore}`);
-  }
-}
 
-playGame();z
